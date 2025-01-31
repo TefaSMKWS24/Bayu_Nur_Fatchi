cd8fae49bfec45d464b6bddbbee7f3c504cbd54a
tefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-01-31 10:02:21 +0700

    modified:   app/Http/Controllers/TransaksiController.php

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -1,10 +1,21 @@
-<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class TransaksiController extends Controller
+return new class extends Migration
 {
-    //
-}
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->string('kode_transaksi', 6)->primary();
+            $table->date('tanggal_transaksi');
+            $table->string('kode_kasir', 6);
+            $table->string('kode_pelanggan', 6);
+            $table->string('kode_voucher', 6)->nullable();
+            $table->string('diskon', 6)->nullable();
+            $table->bigInteger('total_belanja');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
